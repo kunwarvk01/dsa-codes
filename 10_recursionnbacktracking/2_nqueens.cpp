a7215ae53dc7e70254fe9abec5e5644deea9a6dd
kunwarvk01 <kunwarvk01@gmail.com>
2022-10-14 17:20:09 +0530

    10

---
+++ 10_recursionnbacktracking/2_nqueens.cpp
@@ -0,0 +1,10 @@
+/*
+    Problem Statement: The n-queens is the problem of placing n queens on n × n
+    chessboard such that no two queens can attack each other. Given an integer n,
+    return all distinct solutions to the n -queens puzzle. Each solution contains a
+    distinct boards configuration of the queen’s placement, where ‘Q’ and ‘.’ indicate
+    queen and empty space respectively.
+
+    Input: n = 4
+    Output: [["-Q--.","---Q","Q---","--Q-"],["--Q-","Q---","---Q","-Q--"]]
+ */
