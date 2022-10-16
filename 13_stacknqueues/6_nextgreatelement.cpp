596e0b57619d8d12cb90b009cdead3b0f3f67c00
kunwarvk01 <kunwarvk01@gmail.com>
2022-10-16 11:30:00 +0530

    stacks and queues added

---
+++ 13_stacknqueues/6_nextgreatelement.cpp
@@ -0,0 +1,36 @@
+/*
+    Problem Statement: Given a circular integer array A, return the next
+    greater element for every element in A. The next greater element for
+    an element x is the first element greater than x that we come across while traversing the array in a clockwise manner. If it doesn’t exist, return -1 for this element.
+
+    Input: N = 11, A[] = {3,10,4,2,1,2,6,1,7,2,9}
+    Output: 10,-1,6,6,2,6,7,7,9,9,10
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> nextGreaterElements(vector<int> &nums) {
+	int n = nums.size();
+	vector<int> nge(n,-1);
+	stack<int> st;
+	for (int i=2*n-1; i >= 0; i--) {
+		while (!st.empty() && st.top() <= nums[i%n])
+			st.pop();
+
+		if (i < n)
+			if (!st.empty())
+				nge[i] = st.top();
+
+		st.push(nums[i % n]);
+	}
+	return nge;
+}
+
+int main() {
+	vector < int > v {5,7,1,2,6,0};
+	vector < int > res = nextGreaterElements(v);
+	cout << "The next greater elements are" << endl;
+	for (int i = 0; i < (int)res.size(); i++) {
+		cout << res[i] << " ";
+	}
+}
