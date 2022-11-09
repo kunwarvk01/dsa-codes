feb86663ad1d8fb147c9af3f06738b9aa87a53b1
kunwarvk01 <kunwarvk01@gmail.com>
2022-11-09 19:21:25 +0530

    All Permutations 2nd Approach

--- striver-playlist/recursion/8_AllPermutations.cpp
+++ striver-playlist/recursion/8_AllPermutations.cpp
@@ -3,6 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// (!n)
 void recurPermute(vector<int> &ds, vector<int> &nums, vector<vector<int> > &ans, int freq[]){
 	if(ds.size()==nums.size()) {
 		ans.push_back(ds);
@@ -19,9 +20,25 @@ void recurPermute(vector<int> &ds, vector<int> &nums, vector<vector<int> > &ans,
 	}
 }
 
+//Better Space Complexity
+void recurPermutee(int index, vector<int> &nums, vector<vector<int> > &ans){
+	if(index==(int)nums.size()) {
+		ans.push_back(nums);
+		return;
+	}
+	for(int i=index; i<(int)nums.size(); i++) {
+		swap(nums[index], nums[i]);
+		recurPermutee(index+1,nums,ans);
+		swap(nums[index], nums[i]);
+	}
+}
+
 vector<vector<int> > permute(vector<int> &nums){
 	vector<vector<int> > ans;
 	vector<int> ds;
 	int freq[nums.size()]={0};
 	recurPermute(ds, nums, ans, freq);
+	recurPermutee(0, nums, ans);
+
+	return ans;
 }
