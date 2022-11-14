ebab875a1990219a872eaacd6f5aa13e2cbc18c2
kunwarvk01 <kunwarvk01@gmail.com>
2022-11-14 12:35:47 +0530

    kth perm

--- striver-playlist/recursion/13_ratinamaze.cpp
+++ striver-playlist/recursion/13_ratinamaze.cpp
@@ -7,10 +7,10 @@ void solve(int i,int j,vector<vector<int> > &a,int n, vector<string> &ans,string
 		return;
 	}
 	string dir="DLRU";
-	for(int ind=0; i<4; ind++) {
+	for(int ind=0; ind<4; ind++) {
 		int nexti=i+di[ind];
 		int nextj=j+dj[ind];
-		if(nexti>=0 and nextj>=0 and nexti<=n and nextj<=n and !vis[nexti][nextj] and a[nexti][nextj]==1) {
+		if(nexti>=0 && nextj>=0 && nexti<=n && nextj<=n && !vis[nexti][nextj] && a[nexti][nextj]==1) {
 			vis[i][j] = 1;
 			solve(nexti, nextj, a, n,ans,move+dir[ind],vis,di,dj);
 			vis[i][j] = 0;
