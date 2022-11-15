8af6c4059aa82e2bcf9e86bce8e709b653ad516c
kunwarvk01 <kunwarvk01@gmail.com>
2022-11-15 13:22:44 +0530

    graph intro

---
+++ striver-playlist/graphs/1_intro.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int n,m;
+	//adjacency matrix (n^2)
+	int adj[n+1][m+1];
+	for(int i=0; i<n; i++) {
+		int u,v;
+		cin>>u>>v;
+		adj[u][v]=1; //=weight
+		adj[v][u]=1;
+	}
+
+	//adjacency list (2e)
+	// for weighted graphs vector<pair<int,int>> adj
+	vector<int> adjl[m+1];
+	for(int i=0; i<m; i++) {
+		int u,v;
+		cin>>u>>v;
+		adjl[u].push_back(v);
+		adjl[v].push_back(u);
+	}
+	return 0;
+}
