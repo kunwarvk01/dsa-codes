6dc284dcad65cd83204e4c654764d59b003be218
kunwarvk01 <kunwarvk01@gmail.com>
2022-12-18 20:04:13 +0530

    g27

---
+++ striver-playlist/graphs/27_dijkstrasalgorithm.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// priority_queue approach
+// will not work in negetive cycle / negetive weight
+// as it fill fall in infinite loop
+vector<int> dijkstra(int V, vector<vector<int> > adj[], int S){
+	priority_queue<pair<int, int>,vector<pair<int, int> >,greater<pair<int, int> > > pq;
+	vector<int> dist(V,1e9);
+
+	dist[S]=0;
+	pq.push({0,S});
+
+	while(!pq.empty()) {
+		int dis=pq.top().first;
+		int node=pq.top().second;
+		pq.pop();
+
+		for(auto it : adj[node]) {
+			int edgeWeight = it[1];
+			int adjNode = it[0];
+
+			if(dis+edgeWeight < dist[adjNode]) {
+				dist[adjNode] = dis+edgeWeight;
+				pq.push({dist[adjNode],adjNode});
+			}
+		}
+	}
+	return dist;
+}
