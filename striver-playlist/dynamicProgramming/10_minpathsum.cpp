0e2c38a62b8ef06cbfa9f6a4f34dee1ff3082eb3
kunwarvk01 <kunwarvk01@gmail.com>
2022-12-27 12:28:25 +0530

    ds

--- striver-playlist/dynamicProgramming/10_minpathsum.cpp
+++ striver-playlist/dynamicProgramming/10_minpathsum.cpp
@@ -36,18 +36,19 @@ int f3(int n, int m, vector<vector<int> > &matrix){
 	vector<vector<int> > dp(n,vector<int>(m,0));
 	for(int i=0; i<n ; i++) {
 		for(int j=0; j<m; j++) {
-			if(i==0 && j==0) dp[i][j] = matrix[i][j];
-			else{
-				int up = matrix[i][j];
-				if(i>0) up += dp[i-1][j];
-				else up += 1e9;
-
-				int left = matrix[i][j];
-				if(j>0) left+=dp[i][j-1];
-				else left += 1e9;
-
-				dp[i][j] = min(up,left);
+			if(i==0 && j==0) {
+				dp[i][j]= matrix[i][j];
+				continue;
 			}
+			int up = matrix[i][j];
+			if(i>0) up += dp[i-1][j];
+			else up += 1e9;
+
+			int left = matrix[i][j];
+			if(j>0) left+=dp[i][j-1];
+			else left += 1e9;
+
+			dp[i][j] = min(up,left);
 		}
 	}
 	return dp[n-1][m-1];
@@ -60,18 +61,19 @@ int f4(int n, int m, vector<vector<int> > &matrix){
 	for(int i=0; i<n ; i++) {
 		vector<int> temp(m,0);
 		for(int j=0; j<m; j++) {
-			if(i==0 && j==0) temp[j] = matrix[i][j];
-			else{
-				int up = matrix[i][j];
-				if(i>0) up += prev[j];
-				else up += 1e9;
-
-				int left = matrix[i][j];
-				if(j>0) left+=temp[j-1];
-				else left += 1e9;
-
-				temp[j] = min(up,left);
+			if(i==0 && j==0) {
+				temp[j] = matrix[i][j];
+				continue;
 			}
+			int up = matrix[i][j];
+			if(i>0) up += prev[j];
+			else up += 1e9;
+
+			int left = matrix[i][j];
+			if(j>0) left+=temp[j-1];
+			else left += 1e9;
+
+			temp[j] = min(up,left);
 		}
 		prev=temp;
 	}
