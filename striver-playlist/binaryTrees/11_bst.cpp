eec669a1f1812a3168c75e73f83e77291ad2c05c
kunwarvk01 <kunwarvk01@gmail.com>
2023-01-18 10:57:48 +0530

    t11

--- striver-playlist/binaryTrees/11_bst.cpp
+++ striver-playlist/binaryTrees/11_bst.cpp
@@ -64,3 +64,34 @@ int findFloor(TreeNode* root, int key){
 	}
 	return floor;
 }
+
+
+//////// INORDER SUCCESSOR AND PREDECESSOR IN BST /////////////
+// getting inorder first and searching tc O(n+logn) O(N)
+// perform inorder, the first value > || < target = ans   O(N) O(1)
+// applying logic as it is bst  O(H) O(1)
+
+TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p){
+	TreeNode* successor = NULL;
+	while(root!=NULL) {
+		if(p->data >= root->data)
+			root = root->right;
+		else{
+			successor = root;
+			root = root->left;
+		}
+	}
+	return successor;
+}
+TreeNode* inOrderPredecessor(TreeNode* root, TreeNode* p){
+	TreeNode* predecessor = NULL;
+	while(root) {
+		if(root->data <= p->data) {
+			predecessor = root;
+			root = root->right;
+		}
+		else
+			root = root->left;
+	}
+	return predecessor;
+}
