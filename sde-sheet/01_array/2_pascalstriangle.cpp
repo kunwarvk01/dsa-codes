9d5bcb2d6c41ef95bad1d6c3671bbd868cfafcbc
kunwarvk01 <kunwarvk01@gmail.com>
2023-01-19 16:57:42 +0530

    a2

--- sde-sheet/01_array/2_pascalstriangle.cpp
+++ sde-sheet/01_array/2_pascalstriangle.cpp
@@ -10,29 +10,29 @@
    <-- Brute Force --> O(n^2)
    - use vector<vector<int>> arr;
    - set [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+   - resize array and store sum of previous two in current
  */
 #include <bits/stdc++.h>
 using namespace std;
 vector<vector<int> > generate(int numRows) {
 	vector<vector<int> > r(numRows);
 
-	for (int i = 0; i < numRows; i++) {
+	for(int i=0; i<numRows; i++) {
 		r[i].resize(i + 1);
 		r[i][0] = r[i][i] = 1;
 
-		for (int j = 1; j < i; j++)
-			r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
+		for(int j=1; j<i; j++)
+			r[i][j] = r[i-1][j-1] + r[i-1][j];
 	}
-
 	return r;
 }
 
 
 // to find the element at [5][3]
 // element= (r-1) C (c-1);
-
+//
 // to find ncr
-
+//
 // for(int i=0; i<k; i++) {
 // 	ans*=(n-1);
 // 	ans/=(i+1);
