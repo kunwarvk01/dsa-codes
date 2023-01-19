7a0e3d03d26d6d527b6aa21b73ce43ef02727e18
kunwarvk01 <kunwarvk01@gmail.com>
2023-01-19 16:57:37 +0530

    a1

--- sde-sheet/01_array/1_setmatrixzero.cpp
+++ sde-sheet/01_array/1_setmatrixzero.cpp
@@ -24,18 +24,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 void setZeroes(vector<vector<int> > & matrix) {
 	int rows = matrix.size(), cols = matrix[0].size();
 	vector<int> dummy1(rows,-1), dummy2(cols,-1);
-	for (int i = 0; i < rows; i++)
-		for (int j = 0; j < cols; j++)
-			if (matrix[i][j] == 0) {
+	for(int i=0; i<rows; i++)
+		for(int j=0; j<cols; j++)
+			if(matrix[i][j] == 0) {
 				dummy1[i] = 0;
 				dummy2[j] = 0;
 			}
-
-	for (int i = 0; i < rows; i++)
-		for (int j = 0; j < cols; j++)
+	for(int i=0; i<rows; i++)
+		for (int j=0; j<cols; j++)
 			if (dummy1[i] == 0 || dummy2[j]==0)
 				matrix[i][j] = 0;
 }
@@ -49,11 +49,11 @@ void setZeroes(vector<vector<int> > & matrix) {
  */
 
 void setZeroess(vector<vector<int> > & matrix) {
-	int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
-	for (int i = 0; i < rows; i++) {
+	int col0 = 1, rows=matrix.size(), cols = matrix[0].size();
+	for(int i=0; i<rows; i++) {
 		// checking if 0 is present in the 0th column or not
-		if (matrix[i][0] == 0) col0 = 0;
-		for (int j = 1; j < cols; j++)
+		if(matrix[i][0] == 0) col0 = 0;
+		for(int j=1; j<cols; j++)
 			if (matrix[i][j] == 0) {
 				matrix[i][0] = 0;
 				matrix[0][j] = 0;
@@ -62,12 +62,11 @@ void setZeroess(vector<vector<int> > & matrix) {
 	//traversing in the reverse direction and
 	//checking if the row or col has 0 or not
 	//and setting values of matrix accordingly.
-	for (int i = rows - 1; i >= 0; i--) {
-		for (int j = cols - 1; j >= 1; j--)
-			if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+	for(int i=rows-1; i>=0; i--) {
+		for(int j=cols-1; j>=1; j--)
+			if(matrix[i][0] == 0 || matrix[0][j] == 0)
 				matrix[i][j] = 0;
-			}
-		if (col0 == 0)
+		if(col0 == 0)
 			matrix[i][0] = 0;
 	}
 }
