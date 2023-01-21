9d2b0d3c3f9985e06134f2a93241d6c4d016b9df
kunwarvk01 <kunwarvk01@gmail.com>
2023-01-21 10:20:26 +0530

    a4

--- sde-sheet/01_array/4_kedanealgorithm.cpp
+++ sde-sheet/01_array/4_kedanealgorithm.cpp
@@ -27,19 +27,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxSubArray(vector < int > & nums, vector < int > & subarray) {
+int maxSubArray(vector<int>&nums, vector<int>&subarray) {
 	int msf = INT_MIN, meh = 0;
 	int s = 0;
-	int x=nums.size();
-	for (int i = 0; i < x; i++) {
+	int x = nums.size();
+	for(int i=0; i<x; i++) {
 		meh += nums[i];
-		if (meh > msf) {
+		if(meh > msf) {
 			subarray.clear();
 			msf = meh;
 			subarray.push_back(s);
 			subarray.push_back(i);
 		}
-		if (meh < 0) {
+		if(meh < 0) {
 			meh = 0;
 			s = i + 1;
 		}
