3627dc155d3e8b33124d4c71dde30ac8bb4f702a
kunwarvk01 <kunwarvk01@gmail.com>
2023-01-21 10:20:51 +0530

    a22

--- sde-sheet/02_array/2_mergeoverlappingsubintervals.cpp
+++ sde-sheet/02_array/2_mergeoverlappingsubintervals.cpp
@@ -12,22 +12,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector < pair < int, int > > merge(vector < pair < int, int > > & arr) {
+vector<pair<int, int> > merge(vector<pair<int, int> > &arr){
 	int n = arr.size();
 	sort(arr.begin(), arr.end());
-	vector < pair < int, int > > ans;
+	vector<pair<int, int> > ans;
 
-	for (int i = 0; i < n; i++) {
+	for(int i=0; i<n; i++) {
 		int start = arr[i].first, end = arr[i].second;
 
 		//since the intervals already lies
 		//in the data structure present we continue
-		if (!ans.empty())
-			if (start <= ans.back().second)
+		if(!ans.empty())
+			if(start <= ans.back().second)
 				continue;
 
-		for (int j = i + 1; j < n; j++)
-			if (arr[j].first <= end)
+		for(int j = i+1; j<n; j++)
+			if(arr[j].first <= end)
 				end = max(end, arr[j].second);
 
 		end = max(end, arr[i].second);
@@ -38,23 +38,19 @@ vector < pair < int, int > > merge(vector < pair < int, int > > & arr) {
 
 /*
     <-- Better Approach --> O(NlogN)+O(N).O(NlogN) O(N)
-    - if (arr[i].second > arr[i+1].first) merge the intervals
+    - if (arr[i].secSond > arr[i+1].first) merge the intervals
     - push_back the interval
 
  */
 
-vector < vector < int > > merge(vector < vector < int > > & intervals) {
-
+vector<vector<int> > merge(vector<vector<int> > &intervals) {
 	sort(intervals.begin(), intervals.end());
-	vector < vector < int > > merged;
-	int n=intervals.size();
+	vector<vector<int> > merged;
+	int n = intervals.size();
 
-	for (int i = 0; i < n; i++) {
-		if (merged.empty() || merged.back()[1] < intervals[i][0]) {
-			vector < int > v = {
-				intervals[i][0],
-				intervals[i][1]
-			};
+	for(int i=0; i<n; i++) {
+		if(merged.empty() || merged.back()[1] < intervals[i][0]) {
+			vector<int> v = {intervals[i][0],intervals[i][1]};
 			merged.push_back(v);
 		} else
 			merged.back()[1] = max(merged.back()[1], intervals[i][1]);
