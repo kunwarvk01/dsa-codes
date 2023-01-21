de65dc9a0d73215a26d1dcc21a046593da139f63
kunwarvk01 <kunwarvk01@gmail.com>
2023-01-21 10:20:45 +0530

    a21

--- sde-sheet/02_array/1_rotatematrix.cpp
+++ sde-sheet/02_array/1_rotatematrix.cpp
@@ -17,16 +17,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void rotate(vector < vector < int > > & matrix) {
+void rotate(vector<vector<int> > &matrix) {
 	int n = matrix.size();
 	//transposing the matrix
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < i; j++) {
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<i; j++) {
 			swap(matrix[i][j], matrix[j][i]);
 		}
 	}
 	//reversing each row of the matrix
-	for (int i = 0; i < n; i++) {
+	for (int i=0; i<n; i++) {
 		reverse(matrix[i].begin(), matrix[i].end());
 	}
 }
