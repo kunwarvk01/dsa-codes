725f6fa783eb3e523dbfcd728c761c596bb431c1
kunwarvk01 <kunwarvk01@gmail.com>
2023-01-21 10:20:31 +0530

    a5

--- sde-sheet/01_array/5_sortarray012.cpp
+++ sde-sheet/01_array/5_sortarray012.cpp
@@ -25,18 +25,12 @@ void sortColors(vector<int>& nums) {
 
 	while (mid <= high) {
 		switch (nums[mid]) {
-
-		// If the element is 0
 		case 0:
 			swap(nums[low++], nums[mid++]);
 			break;
-
-		// If the element is 1 .
 		case 1:
 			mid++;
 			break;
-
-		// If the element is 2
 		case 2:
 			swap(nums[mid], nums[high--]);
 			break;
