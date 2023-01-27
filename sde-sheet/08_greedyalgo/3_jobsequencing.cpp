7f84eca64a3a9e1668f4e8fdd570375f933c5241
kunwarvk01 <kunwarvk01@gmail.com>
2023-01-27 20:12:09 +0530

    g3

--- sde-sheet/08_greedyalgo/3_jobsequencing.cpp
+++ sde-sheet/08_greedyalgo/3_jobsequencing.cpp
@@ -4,7 +4,7 @@
     jobs done and the maximum profit that can be obtained. Each job takes a single unit of time
     and only one job can be performed at a time.
 
-    <-- Optimal Approach --> O(nlogn + n^2) O()
+    <-- Optimal Approach --> O(nlogn + nm) O(m)
     The strategy to maximize profit should be to pick up jobs that offer higher profits. Hence we
     should sort the jobs in descending order of profit. Now say if a job has a deadline of 4 we
     can perform it anytime between day 1-4, but it is preferable to perform the job on its last
@@ -31,30 +31,28 @@ bool static comparison(Job a, Job b) {
 	return (a.profit > b.profit);
 }
 //Function to find the maximum profit and the number of jobs done
-pair < int, int > JobScheduling(Job arr[], int n) {
+pair<int,int> JobScheduling(Job arr[], int n) {
 
-	sort(arr, arr + n, comparison);
+	sort(arr, arr+n, comparison);
 	int maxi = arr[0].dead;
-	for (int i = 1; i < n; i++) {
+	for(int i=1; i<n; i++)
 		maxi = max(maxi, arr[i].dead);
-	}
 
 	int slot[maxi + 1];
 
-	for (int i = 0; i <= maxi; i++)
+	for (int i=0; i<=maxi; i++)
 		slot[i] = -1;
 
 	int countJobs = 0, jobProfit = 0;
 
-	for (int i = 0; i < n; i++)
-		for (int j = arr[i].dead; j > 0; j--)
+	for(int i=0; i<n; i++)
+		for(int j=arr[i].dead; j>0; j--)
 			if (slot[j] == -1) {
 				slot[j] = i;
 				countJobs++;
 				jobProfit += arr[i].profit;
 				break;
 			}
-
 	return make_pair(countJobs, jobProfit);
 }
 
