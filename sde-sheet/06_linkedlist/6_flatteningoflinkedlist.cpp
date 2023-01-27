5f18eace4140aa5d6341cb0994d65b6c2eb126d2
kunwarvk01 <kunwarvk01@gmail.com>
2023-01-27 12:40:38 +0530

    ll1

--- sde-sheet/06_linkedlist/6_flatteningoflinkedlist.cpp
+++ sde-sheet/06_linkedlist/6_flatteningoflinkedlist.cpp
@@ -1,12 +1,8 @@
 /*
    Problem Statement: Given a Linked List of size N, where every Node represents a sub-linked-list and contains two pointers:
-
    (i) a next pointer to the next node,
-
    (ii) a bottom pointer to a linked list where this node is head.
-
    Each of the sub-linked-list is in sorted order.
-
    Flatten the Link List such that all the nodes appear in a single level while maintaining the sorted order.
  */
 
