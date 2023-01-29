0e90fa72e8a9204232f9feae35b41640e6414aa4
kunwarvk01 <kunwarvk01@gmail.com>
2023-01-29 22:08:21 +0530

    r7

--- striver-playlist/recursion/07_subset2.cpp
+++ striver-playlist/recursion/07_subset2.cpp
@@ -3,21 +3,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void findSubsets(int index, vector<int> &nums, vector<int> &ds, vector<vector<int> > &ans){
+void findSubsets(int ind, vector<int> &nums, vector<int> &ds, vector<vector<int> >&ans) {
 	ans.push_back(ds);
-	for(int i=index; i<(int)nums.size(); i++) {
-		if(i!=index and nums[i]==nums[i-1]) continue;
-		//take
+	for(int i=ind; i<nums.size(); i++) {
+		if(i != ind && nums[i] == nums[i - 1]) continue;
 		ds.push_back(nums[i]);
-		findSubsets(index+1,nums,ds,ans);
+		findSubsets(i + 1, nums, ds, ans);
 		ds.pop_back();
 	}
 }
-
-vector<vector<int> > subsetNonDuplicates(vector<int> &nums){
+vector<vector<int> > subsetsWithDup(vector<int> &nums) {
 	vector<vector<int> > ans;
 	vector<int> ds;
 	sort(nums.begin(), nums.end());
-	findSubsets(0,nums,ds,ans);
+	findSubsets(0, nums, ds, ans);
 	return ans;
 }
