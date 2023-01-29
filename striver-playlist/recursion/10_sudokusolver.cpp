3d32b109ed45bc1fb6ee2f2652de4d6f87f63859
kunwarvk01 <kunwarvk01@gmail.com>
2023-01-29 22:08:28 +0530

    r10

--- striver-playlist/recursion/10_sudokusolver.cpp
+++ striver-playlist/recursion/10_sudokusolver.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 bool isValid(vector<vector<char> > &board,int row, int col,char c){
-	for(int i = 0; i < 9; i++) {
+	for(int i=0; i<9; i++) {
 		if(board[i][col]==c) return false;
 		if(board[row][i]==c) return false;
 		if(board[3*(row/3)+i/3][3*(col/3)+i%3]) return false;
@@ -11,8 +11,8 @@ bool isValid(vector<vector<char> > &board,int row, int col,char c){
 }
 
 bool solve(vector<vector<char> > &board){
-	for(int i = 0; i<board.size(); i++) {
-		for(int j = 0; j<board[0].size(); j++) {
+	for(int i=0; i<board.size(); i++) {
+		for(int j=0; j<board[0].size(); j++) {
 
 			if(board[i][j]=='.') {
 				for(char c = '1'; c <= '9'; c++) {
