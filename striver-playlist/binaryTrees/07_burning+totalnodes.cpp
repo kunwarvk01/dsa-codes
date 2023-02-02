e306ec6e5b34ae1394b64cc244a2c3e3e94d97dc
kunwarvk01 <kunwarvk01@gmail.com>
2023-02-02 18:47:55 +0530

    bt7

--- striver-playlist/binaryTrees/07_burning+totalnodes.cpp
+++ striver-playlist/binaryTrees/07_burning+totalnodes.cpp
@@ -10,7 +10,57 @@ struct TreeNode {
 	}
 };
 
+//////////// ALL NODES AT DISTANCE K ///////////////////
+vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
+	unordered_map<TreeNode*, TreeNode*> parent_track;     // node -> parent
+	unordered_map<TreeNode*, bool> visited;
+	queue<TreeNode*> q;
+	q.push(root);
+	while(!q.empty()) {     /*First BFS to get a track of parent nodes*/
+		TreeNode* node = q.front();
+		q.pop();
+		if(node->left) {
+			parent_track[node->left] = node;
+			q.push(node->left);
+		}
+		if(node->right) {
+			parent_track[node->right] = node;
+			q.push(node->right);
+		}
+	}
+	q.push(target);
+	visited[target] = true;
+	int curr_level = 0;
+	while(!q.empty()) {     /*Second BFS to go upto K level from target node and using our hashtable info*/
+		int size = q.size();
+		if(curr_level++ == K) break;
+		for(int i=0; i<size; i++) {
+			TreeNode* node = q.front();
+			q.pop();
+			if(node->left && !visited[node->left]) {
+				q.push(node->left);
+				visited[node->left] = true;
+			}
+			if(node->right && !visited[node->right]) {
+				q.push(node->right);
+				visited[node->right] = true;
+			}
+			if(parent_track[node] && !visited[parent_track[node]]) {
+				q.push(parent_track[node]);
+				visited[parent_track[node]] = true;
+			}
+		}
+	}
+	vector<int> result;
+	while(!q.empty()) {
+		result.push_back(q.front()->data);
+		q.pop();
+	}
+	return result;
+}
+
 //////////////// MINIMUM TIME TAKEN TO BURN A BINARY TREE /////////////
+// SAME AS NODES AT K DISTANCE
 // O(N+N+N) O(N)
 int findMaxDistance(map<TreeNode*,TreeNode*> &mp, TreeNode* target){
 	queue<TreeNode*> q;
@@ -75,42 +125,14 @@ int timeToBurnTree(TreeNode* root,int start){
 
 /////////////// NO OF NODES IN A COMPLETE BT /////////////
 // O(N)
-void inOrderTrav(TreeNode* curr, int& count) {
-	if (curr == NULL)
-		return;
-	count++;
-	inOrderTrav(curr->left, count);
-	inOrderTrav(curr->right, count);
-}
-
-// no of nodes in a full tree = 2^h - 1;
-// O(logn * logn) O(logn)
-int findHeightLeft(TreeNode* cur) {
-	int height = 0;
-	while(cur) {
-		height++;
-		cur = cur->left;
-	}
-	return height;
-}
-int findHeightRight(TreeNode* cur) {
-	int height = 0;
-	while(cur) {
-		height++;
-		cur = cur->right;
-	}
-	return height;
-}
 int countNodes(TreeNode* root) {
-	if(root == NULL) return 0;
+	if(!root) return 0;
+	int heightl = 0, heightr = 0;
+	TreeNode* l=root;
+	TreeNode* r=root;
+	while(l) {heightl++; l=l->left;}
+	while(r) {heightr++; r=r->right;}
+	if(heightl==heightr) return pow(2,heightl)-1;
+	return 1+countNodes(root->left)+countNodes(root->right);
 
-	int lh = findHeightLeft(root);
-	int rh = findHeightRight(root);
-	// when height of left and right are equal
-	if(lh == rh) return (1^lh) - 1;
-	// else count nodes traditionally
-	int leftNodes = countNodes(root->left);
-	int rightNodes = countNodes(root->right);
-
-	return 1 + leftNodes + rightNodes;
 }
