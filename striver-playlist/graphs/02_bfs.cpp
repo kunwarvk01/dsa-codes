fec3040628088eaf3dc6b5025aeb54592859b152
kunwarvk01 <kunwarvk01@gmail.com>
2023-02-08 12:20:50 +0530

    g2

--- striver-playlist/graphs/02_bfs.cpp
+++ striver-playlist/graphs/02_bfs.cpp
@@ -4,15 +4,14 @@ using namespace std;
 //(n+2e)(3n)
 vector<int> bfsgraph(int v, vector<int> adj[]){
 	int vis[v]={0};
-	vis[0]=1;
-
 	queue<int> q;
 	q.push(0);
+	vis[0]=1;
 
 	vector<int> bfs;
 
 	while(!q.empty()) {
-		int node=q.front();
+		int node = q.front();
 		q.pop();
 		bfs.push_back(node);
 		for(auto it:adj[node]) {
