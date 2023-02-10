533f23940adc083f2fcda400e3b8fa3817b32844
kunwarvk01 <kunwarvk01@gmail.com>
2023-02-10 14:41:33 +0530

    g7

--- striver-playlist/graphs/07_rottenoranges.cpp
+++ striver-playlist/graphs/07_rottenoranges.cpp
@@ -4,26 +4,23 @@ using namespace std;
 //(n*m)
 //(n*m)
 int orangesRotting(vector<vector<int> > &grid){
-	int n=grid.size();
-	int m=grid[0].size();
-
+	int m=grid.size();
+	int n=grid[0].size();
 	//{{r,c},t};
 	queue<pair<pair<int,int>,int> > q;
-	int vis[n][m]={0};
+	vector<vector<int> > vis(m,vector<int> (n,0));
 	int cntfresh=0;
 
-	for(int i=0; i<n; i++) {
-		for(int j=0; j<m; j++) {
+	for(int i=0; i<m; i++) {
+		for(int j=0; j<n; j++) {
 			if(grid[i][j]==2) {
 				q.push({{i,j},0});
 				vis[i][j] = 2;
 			}
-
 			if(grid[i][j]==1)
 				cntfresh++;
 		}
 	}
-
 	int tm=0;
 	int drow[]={-1,0,+1,0};
 	int dcol[]={0,+1,0,-1};
@@ -34,11 +31,10 @@ int orangesRotting(vector<vector<int> > &grid){
 		int t=q.front().second;
 		tm=max(tm,t);
 		q.pop();
-
-		for (int i =0; i<4; i++) {
+		for (int i=0; i<4; i++) {
 			int nrow=r+drow[i];
 			int ncol=c+dcol[i];
-			if(nrow>=0 && nrow<n && ncol>=0 && ncol<m
+			if(nrow>=0 && nrow<m && ncol>=0 && ncol<n
 			   && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1) {
 				q.push({{nrow,ncol},t+1});
 				vis[nrow][ncol] =2;
@@ -46,10 +42,7 @@ int orangesRotting(vector<vector<int> > &grid){
 			}
 		}
 	}
-
-	if(cnt!=cntfresh) {
+	if(cnt!=cntfresh)
 		return -1;
-	}
-
 	return tm;
 }
