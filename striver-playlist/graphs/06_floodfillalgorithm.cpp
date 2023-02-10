e3319f3e02e59d1b39be43749ee22e90463bedb1
kunwarvk01 <kunwarvk01@gmail.com>
2023-02-10 14:41:28 +0530

    g6

--- striver-playlist/graphs/06_floodfillalgorithm.cpp
+++ striver-playlist/graphs/06_floodfillalgorithm.cpp
@@ -4,25 +4,24 @@ using namespace std;
 //(n*m)
 //(n*m)+(n*m)
 
-void dfs(int row,int col,vector<vector<int> >&ans,vector<vector<int> >image,int newColor, int delRow[],int delCol[],int iniColor){
-	ans[row][col]=newColor;
-	int n=image.size();
-	int m=image[0].size();
+void dfs(int row,int col,vector<vector<int> >&ans,int color,int delRow[], int delCol[], int ini){
+	if(ini==color) return;
+	ans[row][col]=color;
+	int m=ans.size();
+	int n=ans[0].size();
 	for(int i=0; i<4; i++) {
 		int nrow=row+delRow[i];
 		int ncol=col+delCol[i];
-
-		if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m && image[nrow][ncol]==iniColor) {
-			dfs(nrow,ncol,ans,image,newColor,delRow,delCol,iniColor);
-		}
+		if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && ans[nrow][ncol]==ini)
+			dfs(nrow,ncol,ans,color,delRow,delCol,ini);
 	}
 }
 
-vector<vector<int> > floodFill(vector<vector<int> > &image, int sr,int sc,int newColor){
-	int iniColor=image[sr][sc];
-	vector<vector<int> > ans=image;
+vector<vector<int> > floodFill(vector<vector<int> >& image, int sr, int sc, int color) {
+	int ini = image[sr][sc];
+	vector<vector<int> > ans = image;
 	int delRow[]={-1,0,+1,0};
 	int delCol[]={0,+1,0,-1};
-	dfs(sr,sc,ans,image,newColor,delRow,delCol,iniColor);
+	dfs(sr,sc,ans,color,delRow,delCol,ini);
 	return ans;
 }
