e6aa193cfdcf6ccd76997eddca67627e687b0111
kunwarvk01 <kunwarvk01@gmail.com>
2023-02-10 14:41:23 +0530

    g4

--- striver-playlist/graphs/04_noofprovinces.cpp
+++ striver-playlist/graphs/04_noofprovinces.cpp
@@ -2,30 +2,28 @@
 using namespace std;
 
 //(n)+(v+2e)
-// (n)+(n) space
-void dfs(int node, vector<int> list[], int vis[]){
-	vis[node] = 1;
-	for(auto it:list[node]) {
+// 2n space
+void dfs(int node, vector<int> list[], vector<int> &vis){
+	vis[node]=1;
+	for(auto it: list[node])
 		if(!vis[it])
-			dfs(it, list, vis);
-	}
+			dfs(it,list,vis);
 }
 
-int numProvince(vector<vector<int> > adj, int V){
-	vector<int> list[V];
-	//changing adjacency matrix to adjacency list
-	for(int i = 0; i<V; i++) {
-		for (int j = 0; j <V; j++) {
-			if(adj[i][j]==1 && i!=j) {
+int findCircleNum(vector<vector<int> >&isConnected) {
+	int v = isConnected[0].size();
+	vector<int> list[v];
+	for(int i=0; i<v; i++) {
+		for(int j=0; j<v; j++) {
+			if(isConnected[i][j]==1 && i!=j) {
 				list[i].push_back(j);
 				list[j].push_back(i);
 			}
 		}
 	}
-
-	int vis[V]={0};
+	vector<int> vis(v,0);
 	int count=0;
-	for(int i = 0; i < V; i++) {
+	for(int i=0; i<v; i++) {
 		if(!vis[i]) {
 			count++;
 			dfs(i,list,vis);
