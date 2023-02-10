cb6b0eb5777f0132c939f200e7c68b3be8905193
kunwarvk01 <kunwarvk01@gmail.com>
2023-02-10 14:41:45 +0530

    g9

--- striver-playlist/graphs/09_cycleugdfs.cpp
+++ striver-playlist/graphs/09_cycleugdfs.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool dfs(int node, int parent, int vis[],vector<int> adj[]){
+bool dfs(int node, int parent,vector<int> adj[], int vis[]){
 	vis[node]=1;
 	for(auto adjacentNode: adj[node]) {
 		if(!vis[adjacentNode]) {
-			if(dfs(adjacentNode,node,vis,adj)==true)
+			if(dfs(adjacentNode,node,adj,vis)==true)
 				return true;
 		}
 		else if(adjacentNode!=parent) return true;
@@ -17,7 +17,7 @@ bool isCycle(int V, vector<int> adj[]){
 	int vis[V]={0};
 	for(int i=0; i<V; i++) {
 		if(!vis[i]) {
-			if(dfs(i,-1,vis,adj) == true) return true;
+			if(dfs(i,-1,adj,vis) == true) return true;
 		}
 	}
 	return false;
