b5e4981ea126d9e0788ee6b59f15bd23575a26a4
kunwarvk01 <kunwarvk01@gmail.com>
2023-02-12 13:37:19 +0530

    g14

--- striver-playlist/graphs/14_bipartitegraph.cpp
+++ striver-playlist/graphs/14_bipartitegraph.cpp
@@ -5,36 +5,32 @@ using namespace std;
 //graphs with no cycle are bipartite
 //graph with even nodes in cycle are also bipartite
 
-bool check(int start, int V,vector<int> adj[],int color[]){
+bool check(int start, int v,vector<vector<int> > &graph,vector<int> &color){
 	queue<int> q;
 	q.push(start);
-	color[start]=0;
-
+	color[start]=1;
 	while(!q.empty()) {
 		int node=q.front();
 		q.pop();
-
-		for(auto adjacent : adj[node]) {
-			//if not colored, give adjacent opposite color
-			if(color[adjacent] != -1) {
-				color[adjacent] = !color[node];
-				q.push(adjacent);
+		for(auto i : graph[node]) {
+			//if not colored, give i opposite color
+			if(!color[i]) {
+				color[i] = -color[node];
+				q.push(i);
 			}
-			//if adjacent has same color, return false
-			else if(color[adjacent]==color[node])
+			//if i has same color, return false
+			else if(color[i]==color[node])
 				return false;
 		}
 	}
 	return true;
 }
-
-bool isBipartite(int V, vector<int> adj[]){
-	int color[V]={-1};
-	for(int i=0; i<V; i++) {
-		if(color[i]==-1) {
-			if(check(i, V, adj, color)==false)
+bool isBipartite(vector<vector<int> > &graph){
+	int v=graph.size();
+	vector<int> color(v,0);
+	for(int i=0; i<v; i++)
+		if(!color[i])
+			if(check(i, v, graph, color)==false)
 				return false;
-		}
-	}
 	return true;
 }
