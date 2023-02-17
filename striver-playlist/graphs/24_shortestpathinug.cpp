ea1dd1d4845413bd36e26d3a8efcb39e21d21db7
kunwarvk01 <kunwarvk01@gmail.com>
2023-02-17 15:17:19 +0530

    g24

--- striver-playlist/graphs/24_shortestpathinug.cpp
+++ striver-playlist/graphs/24_shortestpathinug.cpp
@@ -7,26 +7,20 @@ vector<int> shortestPath(vector<vector<int> > &edges, int N, int M, int src){
 		adj[it[0]].push_back(it[1]);
 		adj[it[1]].push_back(it[0]);
 	}
-
-	int dist[N]={INT_MAX};
+	vector<int> dist(N,1e9);
 	dist[src]=0;
 	queue<int> q;
 	q.push(src);
-
 	while(!q.empty()) {
 		int node=q.front();
 		q.pop();
-		for(auto it: adj[node]) {
+		for(auto it: adj[node])
 			if(dist[node]+1<dist[it]) {
 				dist[it]=dist[node]+1;
 				q.push(it);
 			}
-		}
 	}
-	vector<int> ans(N,-1);
-	for(int i=0; i<N; i++) {
-		if(dist[i]==INT_MAX)
-			ans[i]=dist[i];
-	}
-	return ans;
+	for(auto &i: dist)
+		if(i==1e9) i=-1;
+	return dist;
 }
