0714dd89f3bea7947a65b72847bfb3b136073200
kunwarvk01 <kunwarvk01@gmail.com>
2023-02-17 15:17:11 +0530

    g23

--- striver-playlist/graphs/23_shortestpathindag.cpp
+++ striver-playlist/graphs/23_shortestpathindag.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void topoSort(int node, vector<pair<int,int> > adj[], int vis[], stack<int> &st){
-	vis[node] = 1;
-	for(auto it : adj[node]) {
-		int v=it.first;
-		if(!vis[v])
-			topoSort(v,adj,vis,st);
+void topo(int node, vector<pair<int,int> > adj[], vector<int> &vis, stack<int> &st){
+	vis[node]=1;
+	for(auto it: adj[node]) {
+		if(!vis[it.first])
+			topo(it.first,adj,vis,st);
 	}
+	st.push(node);
 }
 
-vector<int> shortestPath(int N, int M, vector<int> edges[]){
-	vector<pair<int, int> > adj[N];
+vector<int> shortestPath(int N,int M, vector<vector<int> >& edges){
+	vector<pair<int,int> > adj[N];
 	for(int i=0; i<M; i++) {
 		int u=edges[i][0];
 		int v=edges[i][1];
@@ -19,28 +19,26 @@ vector<int> shortestPath(int N, int M, vector<int> edges[]){
 		adj[u].push_back({v,wt});
 	}
 
-	// find the topoSort
-	int vis[N]={0};
+	vector<int> vis(N,0);
 	stack<int> st;
 	for(int i=0; i<N; i++)
 		if(!vis[i])
-			topoSort(i,adj,vis,st);
+			topo(i,adj,vis,st);
 
-	// do the distance thing
-	vector<int> dist(N);
-	for(int i=0; i<N; i++) dist[i]=1e9;
+	vector<int> dist(N,1e9);
 	dist[0]=0;
 	while(!st.empty()) {
 		int node=st.top();
 		st.pop();
-
-		for(auto it : adj[node]) {
+		for(auto it: adj[node]) {
 			int v=it.first;
 			int wt=it.second;
 
-			if(dist[node]+wt < dist[v])
+			if(dist[node]+wt<dist[v])
 				dist[v]=dist[node]+wt;
 		}
 	}
+	for(auto &i:dist)
+		if(i==1e9) i = -1;
 	return dist;
 }
