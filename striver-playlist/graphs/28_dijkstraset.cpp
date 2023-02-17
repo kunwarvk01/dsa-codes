a7a600c62a93aeee65f5d70b4f5f826d3681567a
kunwarvk01 <kunwarvk01@gmail.com>
2023-02-17 15:17:39 +0530

    g28

--- striver-playlist/graphs/28_dijkstraset.cpp
+++ striver-playlist/graphs/28_dijkstraset.cpp
@@ -9,14 +9,13 @@ vector<int> dijkstra(int V, vector<vector<int> > adj[], int S){
 	set<pair<int,int> > st;
 	vector<int> dist(V,1e9);
 
-
 	dist[S]=0;
 	st.insert({0,S});
 
 	while(!st.empty()) {
 		auto it = *(st.begin());
-		int dis=it.first;
-		int node=it.second;
+		int dis = it.first;
+		int node= it.second;
 		st.erase(it);
 
 		for(auto it : adj[node]) {
