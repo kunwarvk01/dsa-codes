1ef2a238dc3c3ed545ef61a52716690d8db0996a
kunwarvk01 <kunwarvk01@gmail.com>
2023-02-17 15:17:16 +0530

    g25

--- striver-playlist/graphs/25_wordladder.cpp
+++ striver-playlist/graphs/25_wordladder.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 //wordlength*N*26
 
-int wordLadder(string startWord,string targetWord, vector<string>&wordList){
+int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
 	queue<pair<string,int> > q;
-	q.push({startWord,1});
+	q.push({beginWord,1});
 	unordered_set<string> st(wordList.begin(),wordList.end());
-	st.erase(startWord);
+	st.erase(beginWord);
 
 	while(!q.empty()) {
-		string word = q.front().first;
+		string word=q.front().first;
 		int steps=q.front().second;
 		q.pop();
-		if(word==targetWord) return steps;
 
-		for(int i=0; i<(int)word.size(); i++) {
-			char original = word[i];
+		if(word==endWord) return steps;
+
+		for(int i=0; i<word.size(); i++) {
+			char original=word[i];
 			for(char ch='a'; ch<='z'; ch++) {
-				word[i] = ch;
-				//it exists in the set
-				if(st.find(word) != st.end()) {
+				word[i]=ch;
+				if(st.find(word)!=st.end()) {
 					st.erase(word);
 					q.push({word,steps+1});
 				}
